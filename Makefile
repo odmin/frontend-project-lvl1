bd6eccd2b3178a6131c86622519038189d4e1249
saratovtsev <e.saratovtsev@gmail.com>
2021-01-02 14:07:47 +0200

    #4 Eslint

--- Makefile
+++ Makefile
@@ -4,3 +4,5 @@ brain-games: #main entrypoint
 	node bin/brain-games.js
 publish: #test before package publish
 	npm publish --dry-run
+lint: #eslint task
+	npx eslint .
--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
-export default cli => {
-    const name = readlineSync.question('May I have your name? ');
-    console.log("Hello, " + name);
-}
+
+export default () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}`);
+};
